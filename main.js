ec545a819e256e41925d94e320e026327192588c
GalaxyVinita <75704799+GalaxyVinita@users.noreply.github.com>
2021-07-27 11:59:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+prediction_1 = "";
 Webcam.set(
     {
         width:350,
@@ -20,9 +21,54 @@ Webcam.set(
 
     console.log("ml5 version", ml5.version);
 
-classifer = ml5.imageClassifer('https://teachablemachine.withgoogle.com/models/fNeMQzw9e/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/fNeMQzw9e/model.json', modelLoaded);
 
 function modelLoaded()
 {
     console.log("Model Loaded");
 }
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+
+    speak_data_1 = "The first prediction is " + prediction_1;
+    var utterThis =  new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
+}
+
+function identify()
+{
+    img = document.getElementById("selfie");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("p1name").innerHTML = results[0].label;
+
+
+        if(results[0].label == "okay")
+        {
+            document.getElementById("p1gesture").innerHTML = "&#65039;";
+        }
+
+        if(results[0].label == "thumbsup")
+        {
+            document.getElementById("p1gesture").innerHTML = "&#128077;";
+        }
+
+        if(results[0].label == "victory")
+        {
+            document.getElementById("p1gesture").innerHTML = "&#65039;";
+        }
+        prediction_1 = results[0].label;
+        speak()
+    }
+}
+
